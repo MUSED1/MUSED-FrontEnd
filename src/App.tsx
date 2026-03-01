5c9a0d361226efafa3ec7090f99c03fb3029e49e
Roodrrigoo <A01664716@tec.mx>
2026-03-01 13:34:29 -0600

    coments

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
-import { JoinWsb } from './components/JoinWsb.tsx'
-import { HowItWorks } from './components/HowItWorks.tsx'
+// import { JoinWsb } from './components/JoinWsb.tsx'
+// import { HowItWorks } from './components/HowItWorks.tsx'
 import { BrandStory } from './components/BrandStory'
 import { Footer } from './components/Footer'
 import { LoadingScreen } from './components/LoadingScreen'
@@ -19,11 +19,11 @@ import { DinnerCollectionTwo } from './components/DinnerCollectionTwo'
 import { Confirmation } from './components/Confirmation'
 import { SimpleImageUpload } from './components/SimpleImageUpload'
 import { SecondDinner } from './components/SecondDinner'
-import { Events } from './components/Events'
-import { Collection } from './components/Collection'
-import { ThePics } from './components/ThePics'
-import { FAQ } from './components/FAQ'
-import { Reachout } from './components/Reachout'
+// import { Events } from './components/Events'
+// import { Collection } from './components/Collection'
+// import { ThePics } from './components/ThePics'
+// import { FAQ } from './components/FAQ'
+// import { Reachout } from './components/Reachout'
 import { Login } from './components/Login'
 import { Signup } from './components/Signup'
 import { MyUploads } from './components/MyUploads'
@@ -38,8 +38,8 @@ function HomePage() {
             <Header />
             <main>
                 <Hero />
-                <JoinWsb />
-                <HowItWorks />
+                {/* <JoinWsb /> */}
+                {/* <HowItWorks /> */}
                 <BrandStory />
             </main>
             <Footer />
@@ -69,7 +69,7 @@ export function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
-                    <Route path="/events" element={<Events />} />
+                    {/* <Route path="/events" element={<Events />} /> */}
                     <Route path="/collections" element={<Collections />} />
                     <Route path="/diner" element={<Diner />} />
                     <Route path="/about" element={<About />} />
@@ -80,10 +80,10 @@ export function App() {
                     <Route path="/dinner-collection-two" element={<DinnerCollectionTwo />} />
                     <Route path="/confirmation" element={<Confirmation />} />
                     <Route path="/upload-images" element={<SimpleImageUpload />} />
-                    <Route path="/collection" element={<Collection />} />
-                    <Route path="/the-pics" element={<ThePics />} />
-                    <Route path="/faq" element={<FAQ />} />
-                    <Route path="/reachout" element={<Reachout />} />
+                    {/* <Route path="/collection" element={<Collection />} /> */}
+                    {/* <Route path="/the-pics" element={<ThePics />} /> */}
+                    {/* <Route path="/faq" element={<FAQ />} /> */}
+                    {/* <Route path="/reachout" element={<Reachout />} /> */}
                     <Route path="/my-uploads" element={<MyUploads />} />
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/oauth-success" element={<OAuthSuccess />} />
